37d37ecf62d818dd03c1bee0b954663e90c85387
Miguel F <miguel.fernandes@toptal.com>
2022-06-03 22:26:49 -0300

    added Sentry

--- src/plugins/sentry.js
+++ src/plugins/sentry.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import * as Sentry from '@sentry/vue'
 import { Integrations } from '@sentry/tracing'
+import * as router from '../router'
 
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     Vue,
-    dsn: '',
+    dsn: process.env.SENTRY_DSN,
     integrations: [new Integrations.BrowserTracing()],
     logErrors: true,
 
